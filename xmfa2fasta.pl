d13f829d259040c478313d07a08f1807b7d033ad
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-04-09 08:24:34 +0100

    Don't die on missing sequences. Replace sequence with a series of 'missing' characters (default 'N'). Option '--missing' to set character to use for 'missing'.

--- xmfa2fasta.pl
+++ xmfa2fasta.pl
@@ -15,114 +15,139 @@ use POSIX;
 my $mafft  = '/usr/bin/mafft';
 my $prefix = int( rand(99999) );
 my %opts;
-GetOptions( 'a|align' => \$opts{'a'}, 'h|help' => \$opts{'h'}, 'f|file=s' => \$opts{'f'}, 'i|integer_ids' => \$opts{'i'} )
-  or die("Error in command line arguments\n");
+GetOptions(
+	'a|align'       => \$opts{'a'},
+	'h|help'        => \$opts{'h'},
+	'f|file=s'      => \$opts{'f'},
+	'i|integer_ids' => \$opts{'i'},
+	'm|missing=s'   => \$opts{'m'}
+) or die "Error in command line arguments\n";
 my $infile = $opts{'f'};
 
 if ( $opts{'h'} ) {
 	show_help();
 	exit;
 }
+if ( $opts{'m'} && length( $opts{'m'} ) > 1 ) {
+	die "Missing character can only be a single character\n";
+}
 if ( !$infile ) {
 	say "Usage: xmfa2fasta.pl --file <XMFA filename>";
 	say "See xmfa2fasta.pl --help for more options.";
 	exit 1;
 }
-my $seqs = {};
-my ( @ids, @int_ids );
-my $temp_seq;
-my $current_id = '';
-open( my $fh, '<', $infile ) or die "Cannot open file $infile\n";
-my $locus = 0;
-while ( my $line = <$fh> ) {
+main();
+exit;
 
-	if ( $line =~ /^=/ ) {
-		$seqs->{$current_id}->{$locus} = $temp_seq if defined $current_id;
-		$locus++;
-		next;
-	}
-	if ( $line =~ /^>\s*([\d\w\s\|\-\\\/\.\(\),#]+):/ ) {
-		$seqs->{$current_id}->{$locus} = $temp_seq if defined $current_id;
-		if ( $opts{'i'} ) {
-			my $extracted_id = $1;
-			if ( $extracted_id =~ /^(\d+)/ ) {
-				$current_id = $1;
-			} else {
-				die "Invalid identifier with --integer_ids option.\n";
-			}
-		} else {
-			$current_id = $1;
+sub main {
+	my $seqs = {};
+	my ( @ids, @int_ids );
+	my $temp_seq;
+	my $current_id = '';
+	open( my $fh, '<', $infile ) or die "Cannot open file $infile\n";
+	my $locus = 0;
+	while ( my $line = <$fh> ) {
+		if ( $line =~ /^=/ ) {
+			$seqs->{$current_id}->{$locus} = $temp_seq if defined $current_id;
+			$locus++;
+			next;
 		}
-		if ( !$seqs->{$current_id} ) {
-			push @ids, $current_id;
-		}
-		undef $temp_seq;
-	} else {
-		$line =~ s/[\r\n]//g;
-		$temp_seq .= $line;
-	}
-}
-close $fh;
-my $locus_count = $locus;
-if ( $opts{'a'} ) {
-	my $in_file      = "$prefix.fas";
-	my $aligned_file = "$prefix\_aligned.fas";
-	my $aligned_seqs = {};
-	my %int_id_used;
-	foreach my $locus ( 0 .. $locus_count - 1 ) {
-		open( my $fh, '>', $in_file ) || die "Can't write temp file.\n";
-		foreach my $id (@ids) {
-			say $fh ">$id";
-			say $fh $seqs->{$id}->{$locus};
-		}
-		close $fh;
-		system("$mafft --quiet --preservecase $in_file > $aligned_file");
-		my $id;
-		my $seq;
-		open( my $fh_in, '<', $aligned_file ) || die "Can't open aligned file.\n";
-		while ( my $line = <$fh_in> ) {
-			if ( $line =~ /^>\s*([\d\w\s\|\-\\\/\.\(\)]+)$/ ) {
-				my $new_id;
-				if ( $opts{'i'} ) {
-					my $extracted_id = $1;
-					if ( $extracted_id =~ /^(\d+)/ ) {
-						$new_id = $1;
-					} else {
-						die "Invalid identifier with --integer_ids options\n";
-					}
+		if ( $line =~ /^>\s*([\d\w\s\|\-\\\/\.\(\),#]+):/ ) {
+			$seqs->{$current_id}->{$locus} = $temp_seq if defined $current_id;
+			if ( $opts{'i'} ) {
+				my $extracted_id = $1;
+				if ( $extracted_id =~ /^(\d+)/ ) {
+					$current_id = $1;
 				} else {
-					$new_id = $1;
+					die "Invalid identifier with --integer_ids option.\n";
 				}
-				chomp $new_id;
-				if ($seq) {
-					$seq =~ s/[\r\n]//g;
-					$aligned_seqs->{$id}->{$locus} = $seq;
-					undef $seq;
-				}
-				$id = $new_id;
 			} else {
-				$seq .= $line;
+				$current_id = $1;
 			}
+			if ( !$seqs->{$current_id} ) {
+				push @ids, $current_id;
+			}
+			undef $temp_seq;
+		} else {
+			$line =~ s/[\r\n]//g;
+			$temp_seq .= $line;
 		}
-		$seq =~ s/[\r\n]//g;
-		$aligned_seqs->{$id}->{$locus} = $seq;
-		close $fh_in;
 	}
-	$seqs = $aligned_seqs;
-	unlink $in_file;
-	unlink $aligned_file;
+	close $fh;
+	my $locus_count = $locus;
+	if ( $opts{'a'} ) {
+		my $in_file      = "$prefix.fas";
+		my $aligned_file = "$prefix\_aligned.fas";
+		my $aligned_seqs = {};
+		my %int_id_used;
+		foreach my $locus ( 0 .. $locus_count - 1 ) {
+			open( my $fh, '>', $in_file ) || die "Can't write temp file.\n";
+			foreach my $id (@ids) {
+				say $fh ">$id";
+				say $fh $seqs->{$id}->{$locus};
+			}
+			close $fh;
+			system("$mafft --quiet --preservecase $in_file > $aligned_file");
+			my $id;
+			my $seq;
+			open( my $fh_in, '<', $aligned_file ) || die "Can't open aligned file.\n";
+			while ( my $line = <$fh_in> ) {
+				if ( $line =~ /^>\s*([\d\w\s\|\-\\\/\.\(\)]+)$/ ) {
+					my $new_id;
+					if ( $opts{'i'} ) {
+						my $extracted_id = $1;
+						if ( $extracted_id =~ /^(\d+)/ ) {
+							$new_id = $1;
+						} else {
+							die "Invalid identifier with --integer_ids options\n";
+						}
+					} else {
+						$new_id = $1;
+					}
+					chomp $new_id;
+					if ($seq) {
+						$seq =~ s/[\r\n]//g;
+						$aligned_seqs->{$id}->{$locus} = $seq;
+						undef $seq;
+					}
+					$id = $new_id;
+				} else {
+					$seq .= $line;
+				}
+			}
+			$seq =~ s/[\r\n]//g;
+			$aligned_seqs->{$id}->{$locus} = $seq;
+			close $fh_in;
+		}
+		$seqs = $aligned_seqs;
+		unlink $in_file;
+		unlink $aligned_file;
+	}
+	foreach my $id (@ids) {
+		say ">$id";
+		my $seq;
+		foreach my $locus ( 0 .. $locus_count - 1 ) {
+			if ( !defined $seqs->{$id}->{$locus} ) {
+				my $std_length = get_standard_locus_length( $seqs, $locus );
+				die "No alleles defined for locus $locus.\n" if !$std_length;
+				$seqs->{$id}->{$locus} = ( $opts{'m'} // 'N' ) x $std_length;
+			}
+			$seq .= $seqs->{$id}->{$locus};
+		}
+		$seq = line_split($seq);
+		say $seq;
+	}
+	return;
 }
-foreach my $id (@ids) {
-	say ">$id";
-	my $seq;
-	foreach my $locus ( 0 .. $locus_count - 1 ) {
-		if ( !defined $seqs->{$id}->{$locus} ) {
-			die "No sequence data for $id:locus $locus.\n";
+
+sub get_standard_locus_length {
+	my ( $seqs, $locus ) = @_;
+	foreach my $id ( keys %$seqs ) {
+		if ( defined $seqs->{$id}->{$locus} ) {
+			return length $seqs->{$id}->{$locus};
 		}
-		$seq .= $seqs->{$id}->{$locus};
 	}
-	$seq = line_split($seq);
-	say $seq;
+	return 0;
 }
 
 sub line_split {
@@ -169,6 +194,9 @@ ${bold}-i, --integer_ids$norm
     the identifiers.  This can be used to resolve problems caused by
     truncation of names in other algorithms.  Identifiers must begin with
     an integer to use this.
+    
+${bold}-m, --missing$norm ${under}CHARACTER$norm  
+    Missing sequence character.  Default is 'N'.
 HELP
 	return;
 }
